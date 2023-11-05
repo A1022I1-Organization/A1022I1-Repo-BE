b644bf41690d93f188da192b1162c53e7ae975de
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-11-05 15:38:59 +0700

    add TruongTT

--- medical_management/src/main/java/com/example/medical_management/controller/account_controller/RestAccountController.java
+++ medical_management/src/main/java/com/example/medical_management/controller/account_controller/RestAccountController.java
@@ -2,6 +2,9 @@ package com.example.medical_management.controller.account_controller;
 
 import com.example.medical_management.model.account.AccountRole;
 import com.example.medical_management.service.account.AccountService;
+import com.example.medical_management.service.email.EmailService;
+import com.example.medical_management.util.account.EncrytedPasswordUtils;
+import com.example.medical_management.util.password.RandomPassword;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestAccountController {
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping("")
     public ResponseEntity<?> create(@RequestBody AccountRole accountRole) {
         try {
             accountService.addNew(accountRole);
+            accountRole.getAppAccount().setPassword(RandomPassword.generatePassword(12));
+            emailService.sendSimpleMessage(
+                    accountRole.getAppAccount().getGmail(),
+                    "Mật khẩu tài khoản vật tư y tế",
+                    "Mật khẩu mới của bạn: " + accountRole.getAppAccount().getPassword()
+            );
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
