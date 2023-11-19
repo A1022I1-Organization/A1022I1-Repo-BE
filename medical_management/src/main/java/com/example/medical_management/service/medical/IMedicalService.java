f7f6dafe3a8edff8b6105aa08929a0e3b4ba0fba
giahuy16404 <phamhuy1642004@gmail.com>
2023-11-19 22:44:20 +0700

    huy

--- medical_management/src/main/java/com/example/medical_management/service/medical/IMedicalService.java
+++ medical_management/src/main/java/com/example/medical_management/service/medical/IMedicalService.java
@@ -17,14 +17,11 @@ public interface IMedicalService {
     Page<MedicalSupplies> findOldSupplies (Pageable pageable);
     Page<MedicalSupplies> findNewSupplies (Pageable pageable);
     MedicalSupplies findByMedical(Long id);
-<<<<<<< HEAD
-    void delete (MedicalSupplies medicalSupplies);
     List<Object[]> findAllBetweenDays(String lastDateInput);
-=======
+
     void delete (long id);
 
-    List<MedicalSupplies> findExpiredSupplies();
->>>>>>> a98ea2ca8b9f4c9c71ba13e27028fd1fcf10cce3
+
     List<MedicalSupplies> getAllListWithPage(String category, int page);
     boolean checkExitsCode(String code);
 }
